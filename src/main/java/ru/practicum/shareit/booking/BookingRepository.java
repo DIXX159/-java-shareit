e3b840f0354b4bc8cfe7a29ebfa26197c4e621ea
Dixx <dixx@list.ru>
2023-03-02 22:20:45 +0700

    ТЗ 14 на проверку.

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -24,77 +24,77 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     Optional<Booking> findBookingByBookerAndItemAndStartBefore(Long bookerId, Long itemId, LocalDateTime dateTime);
 
-    @Query(value = "select * from bookings as b " +
-            "where booker_id = ?1 " +
-            "group by b.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+    @Query("select b from Booking as b " +
+            "where b.booker = ?1 and " +
+            "b.status <> 'REJECTED' " +
+            "order by b.start DESC")
     List<Booking> findAllByBooker(Long bookerId);
 
-    @Query(value = "select * from bookings as b " +
-            "where booker_id = ?1 and " +
-            "?2 between start_date and end_date " +
+    @Query("select b from Booking as b " +
+            "where b.booker = ?1 and " +
+            "?2 between b.start and b.end " +
             "group by b.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start DESC")
     List<Booking> findCurrentByBooker(Long bookerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b " +
             "where booker_id = ?1 and " +
             "?2 < b.start_date " +
             "group by b.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findFutureByBooker(Long bookerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b " +
             "where booker_id = ?1 and " +
             "?2 > b.end_date " +
             "group by b.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findPastByBooker(Long bookerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b " +
             "where b.status = ?1 and b.booker_id = ?2 " +
             "group by b.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findAllBookingsByStatus(String state, Long bookerId);
 
     @Query(value = "select * from bookings as b " +
             "where b.item_id = ?1 " +
             "group by b.id " +
-            "order by CAST(b.start_date AS date)", nativeQuery = true)
+            "order by b.start_date ", nativeQuery = true)
     List<Booking> findAllBookingsByItem(Long itemId);
 
     @Query(value = "select * from bookings as b join items i on i.id = b.item_id " +
             "where i.owner_id = ?1 " +
             "group by b.id, i.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findAllBookingsByItemsOwner(Long ownerId);
 
     @Query(value = "select * from bookings as b join items i on i.id = b.item_id " +
             "where i.owner_id = ?1 and " +
             "?2 between start_date and end_date " +
             "group by b.id, i.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findCurrentBookingsByItemsOwner(Long ownerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b join items i on i.id = b.item_id " +
             "where i.owner_id = ?1 and " +
             "?2 < b.start_date " +
             "group by b.id, i.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findFutureBookingsByItemsOwner(Long ownerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b join items i on i.id = b.item_id " +
             "where i.owner_id = ?1 and " +
             "?2 > b.end_date " +
             "group by b.id, i.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findPastBookingsByItemsOwner(Long ownerId, LocalDateTime localDateTime);
 
     @Query(value = "select * from bookings as b join items i on i.id = b.item_id " +
             "where i.owner_id = ?1 and " +
             "?2 = b.status " +
             "group by b.id, i.id " +
-            "order by CAST(b.start_date AS date) DESC", nativeQuery = true)
+            "order by b.start_date DESC", nativeQuery = true)
     List<Booking> findAllBookingsByItemsOwnerStatus(Long ownerId, String state);
 
     Booking findBookingByBookerAndItemAndStart(Long bookerId, Long item, LocalDateTime start);
